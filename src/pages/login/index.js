0d749aefa1bd7b94ba83e54d5df1ccbfdac4b2b8
jaysonadam <jaysonadam@gmail.com>
2022-02-23 15:18:16 +0700

    Create Login container design

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,12 @@
+import { Container } from "@mui/material";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "../../config/axios";
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { TextField } from "@mui/material";
 
 function Login() {
     const nis = useSelector((state) => state.auth.nis);
@@ -34,7 +40,74 @@ function Login() {
     };
 
     return (
+        <Container
+            sx={{
+                width: 500,
+                height: 500,
+                backgroundColor: "white",
+                marginTop: 10,
+                borderRadius: 4
+            }}>
+                <Box
+                    sx={{
+                        color: "black",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        fontSize: 20,
 
+                        paddingTop: 4,
+                    }}>Login to your account here!
+                </Box>
+                <Container
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "center",
+                        backgroundColor: "white",
+                        paddingTop: 2,
+                    }}>
+                    <TextField
+                        id="outlined-basic"
+                        label="Username"
+                        variant="outlined"
+                        name="username" // reference ke onLoginClick
+                        onChange={handleChange}
+                        onKeyPress={onInputPress}   
+                        sx={{
+                            p: 1,
+                            m: 1,
+                        }}
+                    />
+                    <TextField 
+                        id="outlined-basic"
+                        label="Password"
+                        variant="outlined"
+                        name="password" // reference ke onLoginClick
+                        type="password"
+                        onChange={handleChange}
+                        onKeyPress={onInputPress}
+                        sx={{
+                            p: 1,
+                            m: 1,
+                        }}
+                    />
+                    <Container
+                        sx={{
+                            marginTop: 3,
+                            display: "flex",
+                            flexWrap: "wrap",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Button 
+                            variant="contained"
+                            color="success"
+                            onClick={onLoginClick}
+                        >Login
+                        </Button>
+                    </Container>
+                </Container>
+            </Container>
     )
 };
 
